c750700c89beea18518ef3b8c4127fc26a8f9282
haimnewclass <56435506+haimnewclass@users.noreply.github.com>
2022-05-10 13:19:28 +0300

    Update App.js
    1

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput,Button,ScrollView } from 'react-native';
+import  {useState} from 'react';
 
 export default function App() {
+
+
+  const [arr,setArr]=useState([]);
+
+  const [txt,setTxt]=useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.topView}>
+      <View>
+      <TextInput style={styles.txtInput} placeholder="Please enter yout Action" value={txt} onChangeText={(t)=>{setTxt(t)}} />
+      </View>
+      <View>
+      <Button title="Add To List" onPress={()=>{
+        // add to list
+
+        setArr([...arr,txt])
+
+      }} />
+      </View>
+      </View>
+      <View>
+        <ScrollView>
+          {arr.map((o,i)=>{
+            <View key={i}>
+            <Text >{o}</Text>
+            
+            </View>
+          })}
+        </ScrollView>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +45,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  },topView:{
+    backgroundColor:'#ccc',
+    height:100
+  },txtInput:{
+    width:100,
+    backgroundColor:'#ff0000',
+    borderWidth:1,
+    borderColor:'#000'
+  }
 });
